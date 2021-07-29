8847e4b4ad9869cabd8b2380aa47de9bbebaadd5
wei yan <yanweigo@outlook.com>
2021-07-29 15:43:56 +0800

    day1 commit
    day1 commit

---
+++ 01.day1/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 文档注释，可以被javadoc所解析
+ * @author yanwei
+ *
+ */
+
+
+// 单行注释
+/*
+ * 多行注释
+ * 
+ */
+public class HelloWorld {
+
+	public static void main(String[] arguments) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ 01.day1/Test1.java
@@ -0,0 +1,10 @@
+
+public class Test1 {
+	public static void main(String[] argus) {
+		System.out.println("姓名： 牛魔王\n");
+//		System.out.println();
+		System.out.println("性别：男");
+		System.out.println("家庭住址：火云洞");
+		
+	}
+}
